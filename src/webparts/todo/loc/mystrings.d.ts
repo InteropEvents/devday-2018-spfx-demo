68a27eee20c88133b3e3546b7f5e399346a19880
Junle Li <lijunle@gmail.com>
2018-11-16 15:52:26 +0800

    Check-in everything from SharePoint generator.

---
+++ src/webparts/todo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITodoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TodoWebPartStrings' {
+  const strings: ITodoWebPartStrings;
+  export = strings;
+}
