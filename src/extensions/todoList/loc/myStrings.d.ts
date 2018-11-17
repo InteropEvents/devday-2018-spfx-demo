4c035f67294ac40b979d6900bf3c203474a61144
Junle Li <lijunle@gmail.com>
2018-11-17 11:15:47 +0800

    Generate the extension template.
    - Update the serve.config to page URL.

---
+++ src/extensions/todoList/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ITodoListFieldCustomizerStrings {
+  Title: string;
+}
+
+declare module 'TodoListFieldCustomizerStrings' {
+  const strings: ITodoListFieldCustomizerStrings;
+  export = strings;
+}
